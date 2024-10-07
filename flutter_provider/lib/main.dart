c46bcb48cc9409ebeb1f4930b4eecf528bce5d35
Rosimaltasari <167852703+Rosimaltasari@users.noreply.github.com>
2024-10-07 12:54:37 +0700

    flutter_provider

---
+++ flutter_provider/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/providers/all_product.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
+    );
+  }
+}
